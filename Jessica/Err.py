e91ace8be9fd25b426ec341b4941f53c4c76c37f
LF <xiayanzheng@outlook.com>
2019-07-17 17:53:42 +0800

    更新lfcomlib\Err 修复了无用户也可以访问NovaAdmin的Bug

--- Jessica/Err.py
+++ Jessica/Err.py
@@ -34,8 +34,6 @@ def logger(**kwargs):
         log.setLevel(log_level)
         log.addHandler(handler)
 
-        logging.basicConfig(filename=log_file, format=log_format, level=log_level)
-
         @wraps(func)
         def wrapper(*args, **kwargs):
             return func(*args, **kwargs)
