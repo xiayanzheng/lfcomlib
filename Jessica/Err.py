fe47833d2aad145caed90dd795d29dd0f1daf2b9
LF <xiayanzheng@outlook.com>
2019-07-26 19:38:05 +0800

    Merge Omika updates Fix Err.logger Bug improve OneKeyBuild\Build.py script

--- Jessica/Err.py
+++ Jessica/Err.py
@@ -14,6 +14,7 @@ def logger(**kwargs):
         log_revision = kwargs["log_revision"]
         log_size = kwargs["log_size"]
         log_level_dict = {
+            "DEBUG":logging.DEBUG,
             "INFO": logging.INFO,
             "WARNING": logging.WARNING,
             "ERROR": logging.ERROR,
@@ -23,28 +24,35 @@ def logger(**kwargs):
             log_level = log_level_dict[level]
         log_name = name if name else func.__module__
         log_file = "{}{}".format(log_path, '.log')
-        handler = RotatingFileHandler(log_file, mode='a', maxBytes=int(log_size) * 1024 * 1024,
-                                         backupCount=int(log_revision), encoding=None, delay=0)
-
-        log = logging.getLogger(log_name)
+        log = logging.getLogger()
         if len(log.handlers) == 0:
+            # Log to file and console
             log_formatter = logging.Formatter(log_format)
-            handler.setFormatter(log_formatter)
-            handler.setLevel(log_level)
-            log.addHandler(handler)
-        logging.basicConfig(filename=log_file, format=log_format, level=log_level)
+            console = logging.StreamHandler()
+            console.setLevel(log_level)
+            console.setFormatter(log_formatter)
+            log.addHandler(console)
+            file = RotatingFileHandler(log_file, mode='a', maxBytes=int(log_size) * 1024 * 1024,
+                                       backupCount=int(log_revision), encoding=None, delay=0)
+            file.setFormatter(log_formatter)
+            file.setLevel(log_level)
+            log.addHandler(file)
+
+        # logging.basicConfig(filename=log_file, format=log_format, level=log_level)
 
         @wraps(func)
         def wrapper(*args, **kwargs):
             log_msg_i = message if message else func.__name__
+            func_object = func(*args, **kwargs)
             try:
-                if level == logging.DEBUG:
+                if log_level_dict[level] == logging.DEBUG:
                     log.log(log_level, log_msg_i)
-                return func(*args, **kwargs)
+                    print(func_object)
+                return func_object
             except Exception as e:
-                print(e)
-                log_msg_i = log_msg_i + e
+                log_msg_i = log_msg_i + str(e)
                 log.log(log_level, log_msg_i)
+                print(print(func_object))
 
         return wrapper
 
