415c5f266603287f98c117c912857042dffd36e4
LF <xiayanzheng@outlook.com>
2019-08-01 17:47:01 +0800

    Err func improve

--- Jessica/Err.py
+++ Jessica/Err.py
@@ -29,27 +29,34 @@ class LogSet:
             set_level_every_time = False
             if 'set_level_every_time' in kwargs.keys():
                 set_level_every_time = kwargs["set_level_every_time"]
+                level = kwargs['set_level_every_time_level']
             if level in self.log_level_dict.keys():
                 log_level = self.log_level_dict[level]
             log_file = "{}{}".format(log_path, '.log')
+            # Create log object for logger and logger_i
             log = logging.getLogger()
+            # Create Log handlers(file and console)
+            log_formatter = logging.Formatter(log_format)
+            console = logging.StreamHandler()
+            console.setLevel(log_level)
+            console.setFormatter(log_formatter)
+            file = RotatingFileHandler(log_file, mode='a', maxBytes=int(log_size) * 1024 * 1024,
+                                       backupCount=int(log_revision), encoding=None, delay=0)
+            file.setFormatter(log_formatter)
+            file.setLevel(log_level)
+            # Add handlers for logger
             if len(log.handlers) == 0:
-                # Log to file and console
-                log_formatter = logging.Formatter(log_format)
-                console = logging.StreamHandler()
-                console.setLevel(log_level)
-                console.setFormatter(log_formatter)
                 log.addHandler(console)
-                file = RotatingFileHandler(log_file, mode='a', maxBytes=int(log_size) * 1024 * 1024,
-                                           backupCount=int(log_revision), encoding=None, delay=0)
-                file.setFormatter(log_formatter)
-                file.setLevel(log_level)
                 log.addHandler(file)
-            log_set.log_core = log
-            log_set.log_core_level = log_level
-            if set_level_every_time is True:
-                return log
-
+                log_set.log_core = log
+                log_set.log_core_level = log_level
+            # Add handlers for logger_i
+            if set_level_every_time:
+                ev_log = logging.getLogger()
+                ev_log.handlers = []
+                ev_log.addHandler(console)
+                ev_log.addHandler(file)
+                return ev_log
 
 log_set = LogSet()
 
@@ -61,7 +68,8 @@ def logger():
         message = ""
         log_core = log_set.log_core
         log_level = log_set.log_core_level
-        # print(log_core.__dict__)
+
+        print("wrapper",log_core.__dict__)
 
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -85,20 +93,21 @@ def logger():
     return decorate
 
 
-def logger_i(*args, set_level_every_time=True,):
+def logger_i(level, log_msg, set_level_every_time=True):
     fun_name = inspect.stack()[1][3]
     log_core = log_set.log_core
     log_level = logging.DEBUG
     if set_level_every_time is True:
         log_cfg['set_level_every_time'] = set_level_every_time
-        log_cfg['log_level'] = args[0]
+        log_cfg['set_level_every_time_level'] = level
         log_core = log_set.make_log_core(**log_cfg)
+        print(log_core.__dict__)
     else:
-        if args[0] in log_set.log_level_dict:
-            log_level = log_set.log_level_dict[args[0]]
+        if level in log_set.log_level_dict:
+            log_level = log_set.log_level_dict[level]
         if log_core is None:
             log_set.make_log_core(**log_cfg)
     if log_core is not None:
-        log_core.log(log_level, "{},{}".format(fun_name, str(args[1])))
+        log_core.log(log_level, "{},{}".format(fun_name, str(log_msg)))
     else:
         pass
